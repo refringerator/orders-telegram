181ee896a683c5f767f0e1335cc4d38cd55b4036
petruha <refloader@gmail.com>
2025-03-02 20:15:41 +0300

    Add bot functionality with Telegraf integration and configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignorePatterns: ["bot/"],
+  },
 ];
 
 export default eslintConfig;
